3e9ce7884a38b622c06c3b789b0f6dd46d805fa4
elaineo <elaine.ou@gmail.com>
2018-01-22 10:33:31 -0800

    update files with env vars

--- tweet.js
+++ tweet.js
@@ -4,20 +4,14 @@
   Twitter client stuff
   **********/
 var Twitter = require('twitter');
+
 /* lightningb0t tweeter */
-var CONSUMER_KEY  =  '6unEwBnCn0cJY6X1iERuFFfU8';
-var CONSUMER_SECRET =  'okvES4xTeWZVwGHn9PnUT9yYXXMCSrMYhyoLU5lYV9bZVcdJPR';
-var ACCESS_TOKEN =    '954556662668394497-d9zEVD7bEI8kB0TaVohJd3BbUa6TuNz';
-var ACCESS_TOKEN_SECRET = 'kZt5mwBo2I6QZlVFVYWypXENEWUz4JTyTlszPTc4eg7lZ';
-
 var client = new Twitter({
-  consumer_key: CONSUMER_KEY,
-  consumer_secret: CONSUMER_SECRET,
-  access_token_key: ACCESS_TOKEN,
-  access_token_secret: ACCESS_TOKEN_SECRET
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
-
-const DISCLOSURE = " #paidShill";
 
 module.exports = function(msg) {
   console.log("### LightingBot: " + msg);
